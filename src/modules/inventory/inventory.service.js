85c2a3a414e9c277d74afbd9b561faf82b596f39
Cloneeu <43687030+Cloneeu@users.noreply.github.com>
2026-05-13 16:44:20 -0600

    [FIX]: Corregí otro error de que no se parseaban bien los parámetros de page y limit en los metodos list y listMovements

--- src/modules/inventory/inventory.service.js
+++ src/modules/inventory/inventory.service.js
@@ -5,14 +5,16 @@ export class InventoryService {
   async list(query) {
     const {
       q = '',
-      page = 1,
-      limit = 10
     } = query
 
     // Estas dos lineas son por que express siempre entrega strings en el req.query
     // Entonces hay que parsearlos explícitamente
     const activo = query.activo === 'true' ? true : query.activo === 'false' ? false : undefined
     const lowStock = query.lowStock === 'true'
+
+    // Para parsear bien (asegurarse que son numeros y no strings)
+    const page = Math.max(1, parseInt(query.page, 10) || 1) // Asegura que page sea al menos 1
+    const limit = Math.max(1, parseInt(query.limit, 10) || 10) // Asegura que limit sea al menos 1
 
     const allProducts = await inventoryRepository.findAllProducts()
 
@@ -154,9 +156,11 @@ export class InventoryService {
       q = '',
       productId,
       tipo,
-      page = 1,
-      limit = 10
     } = query
+
+    // Igualmente para parsear bien los parametros de paginación (asegurarse q sean numeros y no strings)
+    const page = Math.max(1, parseInt(query.page, 10) || 1)
+    const limit = Math.max(1, parseInt(query.limit, 10) || 10)
 
     const allMovements = await inventoryRepository.findAllMovements()
 
